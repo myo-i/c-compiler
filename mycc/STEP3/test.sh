9bdfd7f026b38a4764799ea4f61db19563528090
myo-i <Imiyo0521@gmail.com>
2023-06-23 23:30:30 +0900

    ✨ テストとMkaefile

---
+++ mycc/STEP3/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
+
+  cc -o mycc mycc.c
+  ./mycc "$input" > tmp.s
+  cc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 41 " 12 + 34 - 5 "
+
+echo OK
