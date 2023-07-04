e029716e1e543aa08e56e54b505268e8692333cc
myo-i <Imiyo0521@gmail.com>
2023-07-05 02:42:39 +0900

    🎨 パース

--- mycc/8-file-partition/parse.c
+++ mycc/8-file-partition/parse.c
@@ -1,139 +1,165 @@
 #include "mycc.h"
 
-// //
-// // Parser
-// //
+static Node *expr(Token **rest, Token *tok);
+static Node *equality(Token **rest, Token *tok);
+static Node *relational(Token **rest, Token *tok);
+static Node *add(Token **rest, Token *tok);
+static Node *mul(Token **rest, Token *tok);
+static Node *unary(Token **rest, Token *tok);
+static Node *primary(Token **rest, Token *tok);
 
-// typedef enum {
-//   ND_ADD, // +
-//   ND_SUB, // -
-//   ND_MUL, // *
-//   ND_DIV, // /
-//   ND_EQ,  // ==
-//   ND_NE,  // !=
-//   ND_LT,  // <
-//   ND_LE,  // <=
-//   ND_NUM, // Integer
-// } NodeKind;
-
-// // AST node type
-// typedef struct Node Node;
-// struct Node {
-//   NodeKind kind; // Node kind
-//   Node *lhs;     // Left-hand side
-//   Node *rhs;     // Right-hand side
-//   int val;       // Used if kind == ND_NUM
-// };
-
-Node *new_node(NodeKind kind) {
+static Node *new_node(NodeKind kind) {
   Node *node = calloc(1, sizeof(Node));
   node->kind = kind;
   return node;
 }
 
-Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+static Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
   Node *node = new_node(kind);
   node->lhs = lhs;
   node->rhs = rhs;
   return node;
 }
 
-Node *new_num(int val) {
+static Node *new_unary(NodeKind kind, Node *expr) {
+  Node *node = new_node(kind);
+  node->lhs = expr;
+  return node;
+}
+
+static Node *new_num(int val) {
   Node *node = new_node(ND_NUM);
   node->val = val;
   return node;
 }
 
-Node *expr();
-Node *equality();
-Node *relational();
-Node *add();
-Node *mul();
-Node *unary();
-Node *primary();
-
 // expr = equality
-Node *expr() {
-  return equality();
+static Node *expr(Token **rest, Token *tok) {
+  return equality(rest, tok);
 }
 
 // equality = relational ("==" relational | "!=" relational)*
-Node *equality() {
-  Node *node = relational();
+static Node *equality(Token **rest, Token *tok) {
+  Node *node = relational(&tok, tok);
 
   for (;;) {
-    if (consume("=="))
-      node = new_binary(ND_EQ, node, relational());
-    else if (consume("!="))
-      node = new_binary(ND_NE, node, relational());
-    else
-      return node;
+    if (equal(tok, "==")) {
+      node = new_binary(ND_EQ, node, relational(&tok, tok->next));
+      continue;
+    }
+
+    if (equal(tok, "!=")) {
+      node = new_binary(ND_NE, node, relational(&tok, tok->next));
+      continue;
+    }
+
+    *rest = tok;
+    return node;
   }
 }
 
 // relational = add ("<" add | "<=" add | ">" add | ">=" add)*
-Node *relational() {
-  Node *node = add();
+static Node *relational(Token **rest, Token *tok) {
+  Node *node = add(&tok, tok);
 
   for (;;) {
-    if (consume("<"))
-      node = new_binary(ND_LT, node, add());
-    else if (consume("<="))
-      node = new_binary(ND_LE, node, add());
-    else if (consume(">"))
-      node = new_binary(ND_LT, add(), node);
-    else if (consume(">="))
-      node = new_binary(ND_LE, add(), node);
-    else
-      return node;
+    if (equal(tok, "<")) {
+      node = new_binary(ND_LT, node, add(&tok, tok->next));
+      continue;
+    }
+
+    if (equal(tok, "<=")) {
+      node = new_binary(ND_LE, node, add(&tok, tok->next));
+      continue;
+    }
+
+    if (equal(tok, ">")) {
+      node = new_binary(ND_LT, add(&tok, tok->next), node);
+      continue;
+    }
+
+    if (equal(tok, ">=")) {
+      node = new_binary(ND_LE, add(&tok, tok->next), node);
+      continue;
+    }
+
+    *rest = tok;
+    return node;
   }
 }
 
 // add = mul ("+" mul | "-" mul)*
-Node *add() {
-  Node *node = mul();
+static Node *add(Token **rest, Token *tok) {
+  Node *node = mul(&tok, tok);
 
   for (;;) {
-    if (consume("+"))
-      node = new_binary(ND_ADD, node, mul());
-    else if (consume("-"))
-      node = new_binary(ND_SUB, node, mul());
-    else
-      return node;
+    if (equal(tok, "+")) {
+      node = new_binary(ND_ADD, node, mul(&tok, tok->next));
+      continue;
+    }
+
+    if (equal(tok, "-")) {
+      node = new_binary(ND_SUB, node, mul(&tok, tok->next));
+      continue;
+    }
+
+    *rest = tok;
+    return node;
   }
 }
 
 // mul = unary ("*" unary | "/" unary)*
-Node *mul() {
-  Node *node = unary();
+static Node *mul(Token **rest, Token *tok) {
+  Node *node = unary(&tok, tok);
 
   for (;;) {
-    if (consume("*"))
-      node = new_binary(ND_MUL, node, unary());
-    else if (consume("/"))
-      node = new_binary(ND_DIV, node, unary());
-    else
-      return node;
+    if (equal(tok, "*")) {
+      node = new_binary(ND_MUL, node, unary(&tok, tok->next));
+      continue;
+    }
+
+    if (equal(tok, "/")) {
+      node = new_binary(ND_DIV, node, unary(&tok, tok->next));
+      continue;
+    }
+
+    *rest = tok;
+    return node;
   }
 }
 
-// unary = ("+" | "-")? unary
+// unary = ("+" | "-") unary
 //       | primary
-Node *unary() {
-  if (consume("+"))
-    return unary();
-  if (consume("-"))
-    return new_binary(ND_SUB, new_num(0), unary());
-  return primary();
+static Node *unary(Token **rest, Token *tok) {
+  if (equal(tok, "+"))
+    return unary(rest, tok->next);
+
+  if (equal(tok, "-"))
+    return new_unary(ND_NEG, unary(rest, tok->next));
+
+  return primary(rest, tok);
 }
 
 // primary = "(" expr ")" | num
-Node *primary() {
-  if (consume("(")) {
-    Node *node = expr();
-    expect(")");
+static Node *primary(Token **rest, Token *tok) {
+  if (equal(tok, "(")) {
+    Node *node = expr(&tok, tok->next);
+    *rest = skip(tok, ")");
     return node;
   }
 
-  return new_num(expect_number());
+  if (tok->kind == TK_NUM) {
+    Node *node = new_num(tok->val);
+    *rest = tok->next;
+    return node;
+  }
+
+  error_tok(tok, "expected an expression");
+}
+
+Node *parse(Token *tok) {
+  Node *node = expr(&tok, tok);
+  if (tok->kind != TK_EOF)
+    error_tok(tok, "extra token");
+  return node;
 }
