7169eacf6a9291b8788ee30196b28da1d819ae02
myo-i <Imiyo0521@gmail.com>
2023-07-05 02:42:53 +0900

    🎨 アセンブリ出力

--- mycc/8-file-partition/codegen.c
+++ mycc/8-file-partition/codegen.c
@@ -1,59 +1,75 @@
 #include "mycc.h"
 
+static int depth;
 
-void gen(Node *node);
+static void push(void) {
+  printf("  push %%rax\n");
+  depth++;
+}
 
-//
-// Code generator
-//
+static void pop(char *arg) {
+  printf("  pop %s\n", arg);
+  depth--;
+}
 
-void gen(Node *node) {
-  if (node->kind == ND_NUM) {
-    printf("  push %d\n", node->val);
+static void gen_expr(Node *node) {
+  switch (node->kind) {
+  case ND_NUM:
+    printf("  mov $%d, %%rax\n", node->val);
+    return;
+  case ND_NEG:
+    gen_expr(node->lhs);
+    printf("  neg %%rax\n");
     return;
   }
 
-  gen(node->lhs);
-  gen(node->rhs);
-
-  printf("  pop rdi\n");
-  printf("  pop rax\n");
+  gen_expr(node->rhs);
+  push();
+  gen_expr(node->lhs);
+  pop("%rdi");
 
   switch (node->kind) {
   case ND_ADD:
-    printf("  add rax, rdi\n");
-    break;
+    printf("  add %%rdi, %%rax\n");
+    return;
   case ND_SUB:
-    printf("  sub rax, rdi\n");
-    break;
+    printf("  sub %%rdi, %%rax\n");
+    return;
   case ND_MUL:
-    printf("  imul rax, rdi\n");
-    break;
+    printf("  imul %%rdi, %%rax\n");
+    return;
   case ND_DIV:
     printf("  cqo\n");
-    printf("  idiv rdi\n");
-    break;
+    printf("  idiv %%rdi\n");
+    return;
   case ND_EQ:
-    printf("  cmp rax, rdi\n");
-    printf("  sete al\n");
-    printf("  movzb rax, al\n");
-    break;
   case ND_NE:
-    printf("  cmp rax, rdi\n");
-    printf("  setne al\n");
-    printf("  movzb rax, al\n");
-    break;
   case ND_LT:
-    printf("  cmp rax, rdi\n");
-    printf("  setl al\n");
-    printf("  movzb rax, al\n");
-    break;
   case ND_LE:
-    printf("  cmp rax, rdi\n");
-    printf("  setle al\n");
-    printf("  movzb rax, al\n");
-    break;
+    printf("  cmp %%rdi, %%rax\n");
+
+    if (node->kind == ND_EQ)
+      printf("  sete %%al\n");
+    else if (node->kind == ND_NE)
+      printf("  setne %%al\n");
+    else if (node->kind == ND_LT)
+      printf("  setl %%al\n");
+    else if (node->kind == ND_LE)
+      printf("  setle %%al\n");
+
+    printf("  movzb %%al, %%rax\n");
+    return;
   }
 
-  printf("  push rax\n");
+  error("invalid expression");
+}
+
+void codegen(Node *node) {
+  printf("  .globl main\n");
+  printf("main:\n");
+
+  gen_expr(node);
+  printf("  ret\n");
+
+  assert(depth == 0);
 }
