ef2b5674bf28034ac4dad3b1208a573ead4efb29
myo-i <Imiyo0521@gmail.com>
2023-07-05 02:43:30 +0900

    🎨 メイン

--- mycc/8-file-partition/main.c
+++ mycc/8-file-partition/main.c
@@ -4,22 +4,8 @@ int main(int argc, char **argv) {
   if (argc != 2)
     error("%s: invalid number of arguments", argv[0]);
 
-  // Tokenize and parse.
-  user_input = argv[1];
-  token = tokenize();
-  Node *node = expr();
-
-  // Print out the first half of assembly.
-  printf(".intel_syntax noprefix\n");
-  printf(".global main\n");
-  printf("main:\n");
-
-  // Traverse the AST to emit assembly.
-  gen(node);
-
-  // A result must be at the top of the stack, so pop it
-  // to RAX to make it a program exit code.
-  printf("  pop rax\n");
-  printf("  ret\n");
+  Token *tok = tokenize(argv[1]);
+  Node *node = parse(tok);
+  codegen(node);
   return 0;
 }
